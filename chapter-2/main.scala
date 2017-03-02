4d1f2421b1fc47c3f525dda1661bd9286d62075a
Aaron Rice <adrice727@gmail.com>
2017-03-02 15:45:39 -0800

    Add compose.

--- chapter-2/main.scala
+++ chapter-2/main.scala
@@ -27,4 +27,8 @@ object Main {
   def uncurry[A,B,C](f: A => B => C): (A,B) => C  = {
     (a: A, b: B) => f(a)(b)
   }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
 }
