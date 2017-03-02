7fa9ba96208ed6d1bb667d7e441b2d835cf9cbba
Aaron Rice <adrice727@gmail.com>
2017-03-02 15:41:18 -0800

    Add uncurry.

--- chapter-2/main.scala
+++ chapter-2/main.scala
@@ -23,4 +23,8 @@ object Main {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
     (a: A) => (b: B) => f(a, b)
   }
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C  = {
+    (a: A, b: B) => f(a)(b)
+  }
 }
