16b116e6f66c73fa7e7872b9943859210f2605bb
Aaron Rice <adrice727@gmail.com>
2017-03-02 13:54:28 -0800

    Start chapter 2.

---
+++ chapter-2/main.scala
@@ -0,0 +1,22 @@
+import scala.annotation.tailrec
+
+object Main {
+  def fib(n: Int): Int = {
+    @tailrec
+    def next(n: Int, a: Int = 0, b: Int = 1): Int = {
+      if (n == 0) a
+      else next(n - 1, b, a + b)
+    }
+    next(n - 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def next(x: A, xs: Array[A]): Boolean = {
+      if (xs.length == 0) true
+      else if (!ordered(x, xs.head)) false
+      else next(xs.head, xs.tail)
+    }
+    if (as.length == 0) true else next(as.head, as.tail)
+  }
+}
