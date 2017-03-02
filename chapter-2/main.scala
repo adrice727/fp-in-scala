7efd25255e3b72b485a93d0c0ce570de80603554
Aaron Rice <adrice727@gmail.com>
2017-03-02 15:37:11 -0800

    Add curry function.

--- chapter-2/main.scala
+++ chapter-2/main.scala
@@ -19,4 +19,8 @@ object Main {
     }
     if (as.length == 0) true else next(as.head, as.tail)
   }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
+  }
 }
