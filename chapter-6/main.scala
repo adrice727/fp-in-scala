a58911ac05ba08e20e28f250254d4fde81149769
Aaron Rice <adrice727@gmail.com>
2017-04-02 10:41:18 -0700

    Finish chapter 6.

--- chapter-6/main.scala
+++ chapter-6/main.scala
@@ -88,13 +88,13 @@ object RNG {
 
   val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
-//  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => {
-//    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
-//  }
-//
-//  def intsViaSequence(count: Int): Rand[List[Int]] = {
-//    sequence(List.fill(count)(int))
-//  }
+    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => {
+      fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+    }
+
+    def intsViaSequence(count: Int): Rand[List[Int]] = {
+      sequence(List.fill(count)(int))
+    }
 
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
     val (a, rng2) = f(rng)
@@ -113,6 +113,56 @@ object RNG {
   }
 
   def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-    flatMap(ra)(a => map(rb)(b => f(a,b)))
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
   }
+}
+
+import State._
+
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = {
+    flatMap(a => unit(f(a)))
+  }
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+    flatMap(a => sb.map(b => f(a, b)))
+  }
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    sas.foldRight(unit[S, List[A]](List()))((s, acc) => s.map2(acc)(_ :: _))
+  }
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+}
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Candy {
+  def update = (i: Input) => (s: Machine) => (i,s) match {
+    case (_, Machine(_, 0, _)) => s
+    case (Coin, Machine(false, _, _)) => s
+    case (Turn, Machine(true, _, _)) => s
+    case (Coin, Machine(true, candy, coin)) => Machine(false, candy, coin + 1)
+    case (Turn, Machine(false, candy, coin)) => Machine(true, candy - 1, coin)
+  }
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(input map ( modify[Machine] _ compose update))
+    s <- get
+  } yield (s.coins, s.candies)
 }
